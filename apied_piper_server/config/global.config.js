e73ec52ef6ff7500f3c05d600eaf1d2f23ff518e
Jasson Espinosa <jasson.ant.espinosa.rodriguez1@gmail.com>
2022-11-17 17:56:06 -0600

    17/11/2022

--- apied_piper_server/config/global.config.js
+++ apied_piper_server/config/global.config.js
@@ -1,8 +1,7 @@
 module.exports = {
     port: "3000",
     base_path: "/api/",
-    db_uri: "mongodb://localhost/practicas",
-    user: 'PIED',
-    pass: 'HBO_Sillicon33',
-
+    db_uri: "mongodb+srv://admin:contraloria2022@cluster0.rrkgbin.mongodb.net/portalTransparencia?retryWrites=true&w=majority",
+    user: 'adminContraloria',
+    pass: 'Contraloria2022',
 }
