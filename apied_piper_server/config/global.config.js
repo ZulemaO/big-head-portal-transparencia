10463347be265c4109dcc2d8bc9a938ff5a0adc2
Jasson Espinosa <es402815@uaeh.edu.mx>
2022-11-20 19:55:02 -0600

    new version

--- apied_piper_server/config/global.config.js
+++ apied_piper_server/config/global.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     port: "3000",
     base_path: "/api/",
-    db_uri: "mongodb+srv://admin:contraloria2022@cluster0.rrkgbin.mongodb.net/portalTransparencia?retryWrites=true&w=majority",
-    user: 'adminContraloria',
+    db_uri: "mongodb+srv://admin:contraloria2022@cluster0.rrkgbin.mongodb.net/?retryWrites=true",
+    user: 'admin',
     pass: 'Contraloria2022',
+
 }
